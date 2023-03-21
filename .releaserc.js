cfbc2320efb2d4cd9167249ab882a3fcc6792ddb
riskers <gaoyibobobo@gmail.com>
2023-03-22 00:12:13 +0800

    feat: Update Workflow

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,6 @@ const hooks = require('semantic-release-monorepo-hooks');
 const output = hooks();
 
 module.exports = {
-  extends: 'semantic-release-monorepo',
   branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
