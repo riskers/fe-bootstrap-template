4e698b676da43b9eaf3e7f59e9065a732affeda1
riskers <gaoyibobobo@gmail.com>
2023-03-22 14:21:20 +0800

    feat: Mul method

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,3 +1,4 @@
 export const add = (x: number, y: number) => x + y;
 export const sub = (x: number, y: number) => x - y;
 export const div = (x: number, y: number) => x / y;
+export const mul = (x: number, y: number) => x * y;
