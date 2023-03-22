109db9008297c4be68be443d409dde0ea514d802
riskers <gaoyibobobo@gmail.com>
2023-03-22 11:27:43 +0800

    feat: Update

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@
 // const output = hooks();
 
 module.exports = {
+  extends: 'semantic-release-monorepo',
   branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
