6067ea0eca8b77ad7ffe05fc9e44a0583b66c3df
riskers <gaoyibobobo@gmail.com>
2023-03-23 00:22:53 +0800

    feat: Update

--- .releaserc.js
+++ .releaserc.js
@@ -1,44 +1,22 @@
-const hooks = require('semantic-release-monorepo-hooks');
-const output = hooks();
-
 module.exports = {
-  branch: 'master',
-  tagFormat: 'v${version}',
-  prepare: [
-    '@semantic-release/changelog',
+  branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      message: 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'docs/CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
   ],
-  publish: ['@semantic-release/npm'],
-  verifyConditions: ['@semantic-release/npm', '@semantic-release/git'],
-  monorepo: {
-    analyzeCommits: ['@semantic-release/commit-analyzer'],
-    generateNotes: ['@semantic-release/release-notes-generator'],
-  },
 };
-
-// module.exports = {
-//   branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
-//   plugins: [
-//     '@semantic-release/commit-analyzer',
-//     '@semantic-release/release-notes-generator',
-//     [
-//       '@semantic-release/changelog',
-//       {
-//         changelogFile: 'docs/CHANGELOG.md',
-//       },
-//     ],
-//     '@semantic-release/npm',
-//     [
-//       '@semantic-release/git',
-//       {
-//         assets: ['package.json', 'docs/CHANGELOG.md'],
-//         message: 'chore(release): ${nextRelease.version} [skip ci]',
-//       },
-//     ],
-//     '@semantic-release/github',
-//   ],
-// };
