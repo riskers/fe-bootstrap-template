415ddabb7af3e594c0d59280cd22f5d5855c6f6d
riskers <gaoyibobobo@gmail.com>
2023-03-22 22:17:03 +0800

    feat: Update

--- .releaserc.js
+++ .releaserc.js
@@ -1,39 +1,22 @@
-const hooks = require('semantic-release-monorepo-hooks');
-const output = hooks();
-
 module.exports = {
   branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
-  monorepo: {
-    analyzeCommits: ['@semantic-release/commit-analyzer'],
-    generateNotes: ['@semantic-release/release-notes-generator'],
-  },
-  prepare: [
-    '@semantic-release/changelog',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      message: 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'docs/CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
   ],
-  publish: ['@semantic-release/npm'],
-  verifyConditions: ['@semantic-release/npm', '@semantic-release/git'],
-  // plugins: [
-  //   '@semantic-release/commit-analyzer',
-  //   '@semantic-release/release-notes-generator',
-  //   [
-  //     '@semantic-release/changelog',
-  //     {
-  //       changelogFile: 'docs/CHANGELOG.md',
-  //     },
-  //   ],
-  //   '@semantic-release/npm',
-  //   [
-  //     '@semantic-release/git',
-  //     {
-  //       assets: ['package.json', 'docs/CHANGELOG.md'],
-  //       message: 'chore(release): ${nextRelease.version} [skip ci]',
-  //     },
-  //   ],
-  //   '@semantic-release/github',
-  // ],
 };
