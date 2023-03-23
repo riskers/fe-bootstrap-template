e1a439cf359a06e62d3d5c63ac495f510add0247
riskers <gaoyibobobo@gmail.com>
2023-03-23 18:16:35 +0800

    feat: A

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -3,4 +3,4 @@ export const sub = (x: number, y: number) => x - y;
 export const div = (x: number, y: number) => x / y;
 export const mul = (x: number, y: number) => x * y;
 export const toNumber = (x: string) => Number(x);
-// export const toString = (x: number) => String(x);
+export const toString = (x: number) => String(x);
